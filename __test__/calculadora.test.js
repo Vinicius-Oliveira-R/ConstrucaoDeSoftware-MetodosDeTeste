fed320077de13304ece36077f60e679c051edc4d
vinicius <viniciusoliveira12353@gmail.com>
2025-08-05 22:40:47 -0300

    implementacao de mais testes

--- __test__/calculadora.test.js
+++ __test__/calculadora.test.js
@@ -1,7 +1,7 @@
 const CalculadoraServico = require("../src/servico/CalculadoraServico")
 
 
-test("Deve retornar a soa de dois números",()=>{
+test("Deve retornar a soma de dois números",()=>{
 
     // dado
     let a = 10
@@ -12,5 +12,45 @@ test("Deve retornar a soa de dois números",()=>{
     let resposta = CalculadoraServico.somar(a, b)
 
     // então
+    expect(resposta).toBe(saidaEsperada)
+})
+
+
+test("Deve retornar a subtracao de dois numeros", ()=>{
+    let a = 10
+    let b = 20
+    let saidaEsperada = 10
+
+    let resposta = CalculadoraServico.subtracao(a,b)
+
+    expect(resposta).toBe(saidaEsperada)
+})
+
+test("Deve retornar a multiplicação de dois numeros", () => {
+    let a = 10
+    let b = 20
+    let saidaEsperada = 200
+
+    let resposta = CalculadoraServico.multiplicacao(a, b)
+
+    expect(resposta).toBe(saidaEsperada)
+})
+
+test("Deve retornar a divisao de dois numeros", () => {
+    let a = 5
+    let b = 50
+    let saidaEsperada = 10
+
+    let resposta = CalculadoraServico.divisao(a, b)
+
+    expect(resposta).toBe(saidaEsperada)
+})
+
+test("Deve retornar o fatorial um número", () => {
+    let a = 5
+    let saidaEsperada = 120
+
+    let resposta = CalculadoraServico.fatorial(a, b)
+
     expect(resposta).toBe(saidaEsperada)
 })
--- src/servico/CalculadoraServico.js
+++ src/servico/CalculadoraServico.js
@@ -1,3 +1,19 @@
 module.exports = class CalculadoraServico{
-    
+    static soma(a,b){
+        return a+b;
+    }
+    static subtracao(a, b) {
+        return a - b;
+    }
+    static divisao(a, b) {
+        return a / b;
+    }
+    static multiplicacao(a, b) {
+        return a * b;
+    }
+    static fatorial(a) {
+        aux = a;
+        
+        return a
+    }
 }
